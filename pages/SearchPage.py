e3b46a99ae7bccbdbf6dbe5b4fab0cd5ad204a5b
manikhossain27 <manik.hossain_27@outlook.com>
2024-06-28 23:06:19 +0600

    Updated code

--- pages/SearchPage.py
+++ pages/SearchPage.py
@@ -7,6 +7,10 @@ class SearchPage:
         self.driver = driver
 
     valid_hp_product_link_text = "HP LP3065"
+    no_product_message_xpath = "//div[@id='content']/p[2]"
 
     def display_status_of_valid_product(self):
         return self.driver.find_element(By.LINK_TEXT, self.valid_hp_product_link_text).is_displayed()
+
+    def retrive_no_product_message(self):
+        return self.driver.find_element(By.XPATH, self.no_product_message_xpath).text
--- tests/test_Search_Product.py
+++ tests/test_Search_Product.py
@@ -24,13 +24,21 @@ class TestSearchProduct(BasePage):
         # assert self.driver.find_element(By.LINK_TEXT, "HP LP3065").is_displayed()
 
     def test_search_for_a_invalid_product(self):
-        self.driver.find_element(By.NAME, "search").send_keys("Honda")
-        self.driver.find_element(By.XPATH, "//button[@class='btn btn-default btn-lg']").click()
-        expectedText = "There is no product that matches the search criteria."
-        assert self.driver.find_element(By.XPATH, "//div[@id='content']/p[2]").text.__eq__(expectedText)
+        # create an object of HomePage
+        home_page = HomePage(self.driver)
+        # enter product name in search box field
+        home_page.enter_product_into_search_box_field("Honda")
+        # clicking on search button I return the object of SearchPage(self.driver)
+        search_page = home_page.click_on_search_button()
+        expected_text = "There is no product that matches the search criteria."
+        assert search_page.retrive_no_product_message().__eq__(expected_text)
 
     def test_search_without_entering_any_product(self):
-        self.driver.find_element(By.NAME, "search").send_keys("")
-        self.driver.find_element(By.XPATH, "//button[@class='btn btn-default btn-lg']").click()
-        expectedText = "There is no product that matches the search criteria."
-        assert self.driver.find_element(By.XPATH, "//div[@id='content']/p[2]").text.__eq__(expectedText)
+        # create an object of HomePage
+        home_page = HomePage(self.driver)
+        # enter product name in search box field
+        home_page.enter_product_into_search_box_field("")
+        # clicking on search button I return the object of SearchPage(self.driver)
+        search_page = home_page.click_on_search_button()
+        expected_text = "There is no product that matches the search criteria."
+        assert search_page.retrive_no_product_message().__eq__(expected_text)
